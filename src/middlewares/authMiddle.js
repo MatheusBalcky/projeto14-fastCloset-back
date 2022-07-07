dc9ae8fef6811c54c666e00846cd146a11633540
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-07-06 23:00:07 -0300

    Checking if the user has a registration

--- src/middlewares/authMiddle.js
+++ src/middlewares/authMiddle.js
@@ -28,5 +28,22 @@ export async function registerMiddle (req, res, next){
 
 export async function loginMiddle (req, res, next){
     const loginBody = req.body;
+    try {
+        const { error } = loginSchema.validate(registerBody);
+        if(error){
+            return res.status(422).send('Preencha os dados corretamente!')
+        }
+
+        const emailExists = await db.collection('users').findOne({ email: registerBody.email});
+        if(!emailExists){
+            return res.status(409).send('Você ainda não possui uma conta, crie uma!')
+        }
+        
+        next();
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).send(`${error}`)
+    }
 
 }
