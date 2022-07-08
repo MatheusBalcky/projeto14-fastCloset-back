e6bb32d914299c41773513e0b0480df366f5bbec
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-07-08 11:43:11 -0300

    Deploy on heroku

--- src/app.js
+++ src/app.js
@@ -10,4 +10,4 @@ app.use(express.json(), cors());
 app.use(routes);
 
 
-app.listen(process.env.PORT, console.log("Server running on port " + PORT));
+app.listen(process.env.PORT, console.log("Server running on port " + process.env.PORT));
