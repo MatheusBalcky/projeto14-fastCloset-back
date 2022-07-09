0636f803ea6e0e3a801e5ac89294e8ebe91f5718
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-07-08 21:47:35 -0300

    Add and get products

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express'
 import { loginController, registerController } from '../controllers/authControllers.js';
 import { loginMiddle, registerMiddle } from '../middlewares/authMiddle.js';
 import { productIdControl } from '../controllers/productIdControl.js';
-
-import { finishOrderControl, ordersFinishedControl } from '../controllers/finishOrderControl.js';
 import { addProductMiddle } from '../middlewares/productsMiddle.js';
 import { addProductController } from '../controllers/addProductControl.js';
 import { getProductsController } from '../controllers/getProductsController.js';
@@ -16,12 +14,6 @@ router.get('/products', getProductsController)
 router.post('/login', loginMiddle,loginController);
 router.post('/register', registerMiddle, registerController);
 router.get('/product/:id', productIdControl);
-
-
-
-router.post('/finishorder', finishOrderControl);
-router.get('/ordersfinished/:order', ordersFinishedControl);
-
 
 // ! ROTA DE TESTE QND FOR DÁ O DEPLOY
 router.get('/helloworld', (req, res) => {
