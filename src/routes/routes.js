13bd63431b1f9cde7f933d065a8b0518ebd3a3fb
Matheus Balcky <matheuscabal821@gmail.com>
2022-07-08 22:12:41 -0300

    Fixing bug in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,7 @@ import { Router } from 'express'
 import { loginController, registerController } from '../controllers/authControllers.js';
 import { loginMiddle, registerMiddle } from '../middlewares/authMiddle.js';
 import { productIdControl } from '../controllers/productIdControl.js';
-
-import { finishOrderControl, ordersFinishedControl } from '../controllers/finishOrderControl.js';
+import { finishOrderControl, ordersFinishedControl } from '../controllers/ordersControl.js';
 import { addProductMiddle } from '../middlewares/productsMiddle.js';
 import { addProductController } from '../controllers/addProductControl.js';
 import { getProductsController } from '../controllers/getProductsController.js';
@@ -16,9 +15,6 @@ router.get('/products', getProductsController)
 router.post('/login', loginMiddle,loginController);
 router.post('/register', registerMiddle, registerController);
 router.get('/product/:id', productIdControl);
-
-
-
 router.post('/finishorder', finishOrderControl);
 router.get('/ordersfinished/:order', ordersFinishedControl);
 
